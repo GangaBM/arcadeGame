afed2a0f5ca183807b4cc59674c051f6e6031ef3
ganga <m.gangabhavani@gmail.com>
2015-12-02 11:19:20 -0500

    project resubmission

--- frontend-nanodegree-arcade-game-master/js/app.js
+++ frontend-nanodegree-arcade-game-master/js/app.js
@@ -1,4 +1,4 @@
-//*********************** Enemy object and Methods************************************
+//Enemy object and Methods
 var Enemy = function(initX, initY) {
     // Variables applied to each of our instances go here,
     this.sprite = 'images/enemy-bug.png';
@@ -10,33 +10,32 @@ var Enemy = function(initX, initY) {
 // Update the enemy's position by adding speed to initial x coordinate and multiplying by dt.
 
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-
     if (this.x > 480) {
         this.x = Math.floor(Math.random() * 30);
     }
-
-   this.x = this.x + this.speed * 2 * dt;
-
+    this.x = this.x + this.speed * 2 * dt;
 };
 
-// Draw the enemy on the screen, required method for game
+// Draws the enemy on the screen
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-//*********************************** Player Object and methods ****************************************
+//Player Object and methods
 
-var score = 0;
+//Player constructor
 var Player = function(initX, initY) {
     this.x = initX;
     this.y = initY;
     this.sprite = 'images/char-boy.png';
-}
+};
+
+// player movement through handleInput which listens to key presses. Resetting player's position once he reaches water
+//and also when he collides with any of the enemies.
 Player.prototype.update = function() {
-    reset();
+    if (player.y == 17.5) {
+    this.reset();
+    }
     for (var i = 0; i <= allEnemies.length - 1; i++) {
         if (allEnemies[i].x < this.x + 50 &&
             allEnemies[i].x + 50 > this.x &&
@@ -45,42 +44,51 @@ Player.prototype.update = function() {
             this.x = 310;
             this.y = 430;
 
-        } else {
-            Player.prototype.handleInput();
         }
-
     }
+};
 
-
-}
-
+// draws player on screen
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
-}
+};
 
-
+//listens to the key presses and moves the player accordingly
 Player.prototype.handleInput = function(direction) {
-    if (direction == 'left' && this.x > 101) {
+    this.direction = direction;
+    if (this.direction == 'left' && this.x > 101) {
         this.x -= 100;
     }
-    if (direction == 'up' && this.y > 25) {
+    if (this.direction == 'up' && this.y > 25) {
         this.y -= 82.5;
         console.log(this.y);
-   }
-    if (direction == 'right' && this.x < 404) {
+    }
+    if (this.direction == 'right' && this.x < 404) {
         this.x += 100;
     }
-    if (direction == 'down' && this.y < 430) {
+    if (this.direction == 'down' && this.y < 430) {
         this.y += 82.5;
     }
 
-}
+};
 
+var score = 0; //defined to keep track of scores each time the player reaches the water
+//everytime the player reaches the water, he comes to the initial position and score gets updated
+Player.prototype.reset = function() {
+        console.log(player.y+"second");
+        player.x = 310;
+        player.y = 430;
+        ctx.fillStyle = "black";
+        ctx.font = "40 georgia";
+        ctx.fillText("Great!!", 200, 200);
+        score += 100;
+        document.getElementById("score").innerHTML = "SCORE: " + score;
+    };
 
+//Instantiation of Objects
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
+//instantiation of enemy objects
 var allEnemies = [];
 x = 30;
 y = 70;
@@ -89,36 +97,23 @@ for (var i = 0; i <= 2; i++) {
     allEnemies.push(enemy);
     x = Math.floor(Math.random() * 505);
     y += 80;
-}
-// Place the player object in a variable called player
+};
 
+// instantiation of the player object
 var player = new Player(310, 430);
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+
+//Helper/input functions
+// This listens for key presses
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down',
-        32: '(space)'
-    };
+   };
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-function reset() {
-    console.log(player.y);
-    if (player.y == 17.5) {
-        player.x = 310;
-        player.y = 430;
-        ctx.fillStyle = "black";
-        ctx.font = "40 georgia";
-        ctx.fillText("Great!!", 200, 200);
-        score += 100;
-        document.getElementById("score").innerHTML = "SCORE: " + score;
-    }
-}
 
